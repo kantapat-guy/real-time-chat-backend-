c7a2c34506d5b893d8ab17ea31da7b4464bbbb94
kantapat-guy <107095121+kantapat-guy@users.noreply.github.com>
2023-04-08 01:23:07 +0700

    config again

--- api/index.js
+++ api/index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const http = require("http");
 const cors = require("cors");
 const config = require("../config");
 
 const app = express();
-const server = http.createServer(app);
 
 app.use(
   cors({
@@ -20,32 +18,4 @@ if (config.isVercel) {
   });
 }
 
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    optionsSuccessStatus: 200,
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log(`Socket.io was connected ID: ${socket.id}`);
-
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User ID: ${socket.id} joined room ID: ${data}`);
-  });
-
-  socket.on("send_message", (data) => {
-    socket.to(data.roomId).emit("receive_message", data);
-    console.log("message_____sending");
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id);
-  });
-});
-
-server.listen(3001, () => {
-  console.log("Server listening on port: 3001");
-});
+module.exports = app;
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const express = require("express");
 const http = require("http");
-const cors = require("cors");
+const app = require("./api");
 
-const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
